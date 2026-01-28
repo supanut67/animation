c2f28ef12f977cbb1f64d6c6c522b5eeee667c14
supanut67 <s43497@bj.ac.th>
2026-01-28 11:26:01 +0700

    Add files via upload

---
+++ a4_sheep_34/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1769574462,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/point-sheet0.png",
+		"images/player-sheet0.png",
+		"images/pillar-sheet0.png",
+		"images/land-sheet0.png",
+		"images/tiledbackground.png",
+		"images/line.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
