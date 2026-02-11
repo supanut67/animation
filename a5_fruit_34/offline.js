94d82df2476388ef67e0d8a83921aec3ba3385b4
supanut67 <s43497@bj.ac.th>
2026-02-11 10:25:08 +0700

    Add files via upload

---
+++ a5_fruit_34/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1770780450,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/botcut-sheet0.png",
+		"images/fruitslash-sheet0.png",
+		"images/grapebot-sheet0.png",
+		"images/grapetop-sheet0.png",
+		"images/grape-sheet0.png",
+		"images/strawberry-sheet0.png",
+		"images/topcut-sheet0.png",
+		"images/spawner-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
